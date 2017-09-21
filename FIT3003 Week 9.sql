b5e838af5209e222244648507182e43b25d8bbe0
Paarth Bhasin <paarthbhasin4@gmail.com>
2017-09-21 17:42:56 +1000

    Update FIT3003 Week 9.sql

--- FIT3003 Week 9.sql
+++ FIT3003 Week 9.sql
@@ -2,46 +2,46 @@ describe dw.charter_fact;
 describe dw.time;
 describe dw.pilot;
 describe dw.model;
-/* PART A */
-/*1 */
+--------------------------------- PART A ---------------------------------
+/*Q1 */
 SELECT time_year, time_month,
 RANK() OVER (ORDER BY time_year, time_month) AS time_rank
 FROM dw.time;
 
-/*2 */
+/*Q2 */
 SELECT time_year, time_month,
 RANK() OVER (ORDER BY time_year, time_month+0) AS time_rank
 FROM dw.time;
 
-/*3 */
+/*Q3 */
 SELECT mod_code, time_id, sum(tot_char_hours), 
 ROW_NUMBER() OVER (ORDER BY SUM(tot_char_hours)) AS ROW_NUMBER
 from DW.CHARTER_FACT
 Where time_id >= '199601' and time_id <= '199612'
 Group by mod_code, time_id;
 
-/*4 */
+/*Q4 */
 SELECT mod_code, time_id, sum(tot_char_hours), 
 DENSE_RANK() OVER (ORDER BY SUM(TOT_CHAR_HOURS))  as DENSE_RANK
 from DW.CHARTER_FACT
 Where time_id BETWEEN '199601' and '199612'
 Group by mod_code, time_id;
 
-/*5 */
+/*Q5 */
 /*
 - ROW_NUMBER returns a serial number for each row, without any gaps. 
 - The numbers returned by the DENSE_RANK function do not have gaps and always 
   have consecutive ranks. 
 */
 
-/*6 */
+/*Q6 */
 SELECT mod_code, time_id, sum(tot_char_hours), 
 RANK() OVER (ORDER BY SUM(TOT_CHAR_HOURS)) as RANK
 from DW.CHARTER_FACT
 Where time_id BETWEEN '199601' and '199612'
 Group by mod_code, time_id;
 
-/*7 */
+/*Q7 */
 /*
 - ROW_NUMBER returns a serial number for each row, without any gaps. 
 - The numbers returned by the DENSE_RANK function do not have gaps and always 
@@ -49,7 +49,7 @@ Group by mod_code, time_id;
 - The RANK function does not always return consecutive integer.
 */
 
-/*8 */
+/*Q8 */
 SELECT mod_code, time_id, sum(tot_char_hours), 
 RANK() OVER (PARTITION BY mod_code ORDER BY mod_code, SUM(TOT_CHAR_HOURS)) 
 as "RANK_BY_MODEL"
@@ -57,7 +57,7 @@ from DW.CHARTER_FACT
 Where time_id BETWEEN '199601' and '199612'
 Group by mod_code, time_id;
 
-/*9 VERY IMPORTANT!!*/
+/*Q9 VERY IMPORTANT!!*/
 Select t.time_year, c.mod_code, sum(c.tot_fuel) as TOTAL,
 RANK() OVER (PARTITION BY t.time_year ORDER BY t.time_year, SUM(c.tot_fuel) DESC) AS RANK_BY_YEAR,
 RANK() OVER (PARTITION BY c.mod_code ORDER BY c.mod_code, SUM(c.tot_fuel) DESC) AS RANK_BY_MODEL
@@ -66,7 +66,7 @@ Where c.time_id = t.time_id
 Group by t.time_year, c.mod_code
 Order by t.time_year, sum(c.tot_fuel) DESC, c.mod_code;
 
-/*10 VERY IMPORTANT!! */
+/*Q10 VERY IMPORTANT!! */
 SELECT mod_code, mod_name, sum(total), 
 RANK() OVER (ORDER BY SUM(total) DESC) as MYRANK
 FROM 
@@ -83,7 +83,7 @@ Where MY_RANK <= 2
 Group By mod_code, mod_name;
 
 
-/*11 VERY IMPORTANT!!*/
+/*Q11 VERY IMPORTANT!!*/
 Select * From
 (
 Select 
@@ -96,9 +96,9 @@ order by Total DESC
 Where PERCENT_RANK >= 0.9;
 
 
-/* PART B */
+--------------------------------- PART B ---------------------------------
 
-/*1 VERY IMPORTANT!!*/
+/*Q1 VERY IMPORTANT!!*/
 SELECT c.time_id, sum(c.revenue),
 TO_CHAR (SUM(SUM(c.revenue)) OVER (ORDER BY c.time_id ROWS UNBOUNDED PRECEDING),
 '9,999,999,999') AS CUMULATIVE_REV
@@ -107,7 +107,7 @@ WHERE c.time_id BETWEEN '199501' and '199512'
 GROUP BY c.time_id
 ORDER BY c.time_id;
 
-/*2 VERY IMPORTANT!!*/
+/*Q2 VERY IMPORTANT!!*/
 /* Error in question. Needs to mention that we need to do average */
 SELECT c.time_id, sum(c.revenue),
 TO_CHAR (AVG(SUM(c.revenue)) OVER (ORDER BY c.time_id ROWS 2 PRECEDING),
@@ -117,14 +117,14 @@ WHERE c.time_id BETWEEN '199501' and '199512'
 GROUP BY c.time_id
 ORDER BY c.time_id;
 
-/*3 */
+/*Q3 */
 /*
 - Cumulative sums up from the current row to the very first row
 - Moving sums up from the current row to N rows before it, where
   N is specified in "ROWS N PRECEDING"
 */
 
-/*4 MOST IMPORTANT!!!*/
+/*Q4 MOST IMPORTANT!!!*/
 SELECT t.time_year, c.mod_code, sum(c.tot_fuel),
 TO_CHAR (SUM(SUM(c.TOT_FUEL)) OVER (PARTITION BY t.time_year ORDER BY t.time_year ROWS UNBOUNDED PRECEDING),
 '9,999,999,999') AS CUM_FUEL_YEAR,
